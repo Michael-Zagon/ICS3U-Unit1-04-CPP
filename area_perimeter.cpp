7a58a76da60950061b0085594b112da9dd578505
Ubuntu <ubuntu@ip-172-31-7-8.ec2.internal>
2021-09-12 23:27:49 +0000

    created area and perimeter in cpp

---
+++ area_perimeter.cpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2021 Michael Zagon All rights reserved
+
+// Created by: Michael Zagon
+// Created on: September 2021
+// This program calculates the area and perimeter
+// of a rectangle with dimensions 4cm x 6cm
+
+#include <iostream>
+
+int main() {
+    std::cout << "If a rectangle has the dimensions: " << "\n";
+    std::cout << "4cm x 6cm" << "\n";
+    std::cout << "\n";
+    std::cout << "Area is " << (4 * 6) << "cm²." << "\n";
+    std::cout << "Perimeter is " << (2 * (4 + 6)) << "cm." << "\n";
+    std::cout << "\n";
+    std::cout << "Done." << "\n";
+}
